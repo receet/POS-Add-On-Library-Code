cdf4100e9f2a066a8043d744c38288852cff67ac
Walaa Abd Alaziz <43107092+walaaAbdAlaziz@users.noreply.github.com>
2019-05-20 15:33:35 +0300

    Add existing file

--- pos_add_on/src/main/java/com/example/pos_add_on/interfaces/VirtualBeaconActionsInterface.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pos_add_on.interfaces
-
-interface VirtualBeaconActionsInterface {
-    fun virtualBeaconManagerDidEncounterError(title:String, message:String)
-    fun virtualBeaconManagerDidStartAdvertising()
-}
